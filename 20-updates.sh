d8bbec9a311bdb8fad2a3ba88680740f27e5e957
fr3aker <fr3aker@github.com>
2014-09-25 19:58:19 +0200

    updated files, fixed possilbe race condition, created directory structure

--- 20-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Place in /etc/motd.d and drop the .sh suffix.
-
-if [ ! -e /var/tmp/.updates ] ; then
-    /usr/local/sbin/check-updates > /var/tmp/.updates
-fi
-
-cat /var/tmp/.updates
--- z-motd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Place in /etc/portage/postsync.d and drop the .sh suffix.
-# Would be nice if this could also run after every package installation.
-
-rm /var/mtp/.updates
